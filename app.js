d8d2ab3b59d3a20e5ada4fc699a27f4ff710ab30
Daviel Frias Garcia <yfriasgarcia@gmail.com>
2019-08-16 16:59:34 +0200

    using request module to comunicate with api

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
 const request = require('request');
+
+const url = 'https://api.darksky.net/forecast/62b6f3d89534c3cabdee2373423dbd76/37.8267,-122.4233';
+
+request({ url: url }, (error, response) => {
+    const data = JSON.parse(response.body);
+    console.log(data.currently);
+})
