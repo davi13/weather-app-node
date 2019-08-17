51204195f5b427c4c6609c44829bf2ae5740d12b
Daviel Frias Garcia <yfriasgarcia@gmail.com>
2019-08-17 07:18:42 +0200

    app.js: handle the network

--- app.js
+++ app.js
@@ -3,9 +3,12 @@ const request = require('request');
 const url = 'https://api.darksky.net/forecast/62b6f3d89534c3cabdee2373423dbd76/37.8267,-122.4233?';
 
 request({ url: url, json: true }, (error, response) => {
-
-    console.log(`${response.body.daily.data[0].summary} It is currently ${response.body.currently.temperature} out there`);
-    console.log(`There is a  ${response.body.currently.precipProbability}% chance of rain`);
+    if (error) {
+        console.log(`Unable to connect to weather service`);
+    } else {
+        console.log(`${response.body.daily.data[0].summary} It is currently ${response.body.currently.temperature} out there`);
+        console.log(`There is a  ${response.body.currently.precipProbability}% chance of rain`);
+    }
 })
 
 const geocodeUrl = 'https://api.mapbox.com/geocoding/v5/mapbox.places/Market%20Street%20and%20Fremont%20Street.json?types=address&proximity=-122.39738575285674,37.792514711136945&access_token=pk.eyJ1IjoiZGF2aTEzIiwiYSI6ImNqemVqZ3FlOTAyeHUzbXFrbno5aTR1dTYifQ.Kp4gmUGKRfyj30HN2z0X3A';
