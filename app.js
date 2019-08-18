c246000455c8a8fcb79734a1000121049a63e4e3
Daviel Frias Garcia <yfriasgarcia@gmail.com>
2019-08-18 09:29:48 +0200

    utils: factorising the geocode function

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
-
-
 const request = require('request');
+const geocode = require('./utils/geocode');
 
 // const url = 'https://api.darksky.net/forecast/62b6f3d89534c3cabdee2373423dbd76/37.8267,-122.4233?';
 
@@ -15,42 +14,8 @@ const request = require('request');
 //     }
 // })
 
-// const geocodeUrl = 'https://api.mapbox.com/geocoding/v5/mapbox.places/Market%20Street%20and%20Fremont%20Street.json?types=address&proximity=-122.39738575285674,37.792514711136945&access_token=pk.eyJ1IjoiZGF2aTEzIiwiYSI6ImNqemVqZ3FlOTAyeHUzbXFrbno5aTR1dTYifQ.Kp4gmUGKRfyj30HN2z0X3A';
-// request({ url: geocodeUrl, json: true }, (error, response) => {
-//     if (error) {
-//         console.log(`Unable to connect to location service`);
-//     } else if (response.body.features.length === 0) {
-//         console.log(`Unable to find to location`);
-//     }
-//     else {
-//         console.log(`Latitud: ${response.body.features[0].center[0]}`);
-//         console.log(`Longitud: ${response.body.features[0].center[1]}`);
-//     }
 
-
-
-// })
-
-const geocode = (address, callback) => {
-    const url = `https://api.mapbox.com/geocoding/v5/mapbox.places/${address}.json?access_token=pk.eyJ1IjoiZGF2aTEzIiwiYSI6ImNqemVqZ3FlOTAyeHUzbXFrbno5aTR1dTYifQ.Kp4gmUGKRfyj30HN2z0X3A`;
-
-
-    request({ url: url, json: true }, (error, response) => {
-
-        if (error) {
-            callback('Unable to connect to location service', undefined);
-        } else if (response.body.features.length === 0) {
-            callback('Unable to find to location ,Try another seach', undefined)
-        } else {
-            callback(undefined, {
-                latitude: response.body.features[0].center[0],
-                longitud: response.body.features[0].center[1],
-                location: response.body.features[0].place_name
-            })
-        }
-    })
-}
-geocode('12what', (error, data) => {
-    console.log(`Error: ${error}`)
-    console.log(`Data: ${data}`)
+geocode('paris', (error, data) => {
+    console.log('Error: ', error)
+    console.log('Data: ', data)
 })
