11f8d1ac24dae3bf268795b0e80c316ca3f64114
Daviel Frias Garcia <yfriasgarcia@gmail.com>
2019-08-18 09:00:58 +0200

    app.js: fixed the problem with Api URl and finish the function using a callback

--- app.js
+++ app.js
@@ -32,21 +32,25 @@ const request = require('request');
 // })
 
 const geocode = (address, callback) => {
-    //const url = `https://api.mapbox.com/geocoding/v5/mapbox.places/${address}.json?types=address&proximity=-122.39738575285674,37.792514711136945&access_token=pk.eyJ1IjoiZGF2aTEzIiwiYSI6ImNqemVqZ3FlOTAyeHUzbXFrbno5aTR1dTYifQ.Kp4gmUGKRfyj30HN2z0X3A`;
-    const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/Market%20Street%20and%20Fremont%20Street.json?types=address&proximity=-122.39738575285674,37.792514711136945&access_token=pk.eyJ1IjoiZGF2aTEzIiwiYSI6ImNqemVqZ3FlOTAyeHUzbXFrbno5aTR1dTYifQ.Kp4gmUGKRfyj30HN2z0X3A';
+    const url = `https://api.mapbox.com/geocoding/v5/mapbox.places/${address}.json?access_token=pk.eyJ1IjoiZGF2aTEzIiwiYSI6ImNqemVqZ3FlOTAyeHUzbXFrbno5aTR1dTYifQ.Kp4gmUGKRfyj30HN2z0X3A`;
+
 
     request({ url: url, json: true }, (error, response) => {
-        console.log(response.body.features.length);
+
         if (error) {
             callback('Unable to connect to location service', undefined);
         } else if (response.body.features.length === 0) {
-            console.log(response.body.features.length);
             callback('Unable to find to location ,Try another seach', undefined)
-
+        } else {
+            callback(undefined, {
+                latitude: response.body.features[0].center[0],
+                longitud: response.body.features[0].center[1],
+                location: response.body.features[0].place_name
+            })
         }
     })
 }
-geocode('new york', (error, data) => {
+geocode('12what', (error, data) => {
     console.log(`Error: ${error}`)
     console.log(`Data: ${data}`)
 })
