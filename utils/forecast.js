347948be4003a0ea211a86e5e5b73d4aa539483e
Daviel Frias Garcia <yfriasgarcia@gmail.com>
2019-08-20 09:36:40 +0200

    forecast.js: using destructuring

--- utils/forecast.js
+++ utils/forecast.js
@@ -1,13 +1,13 @@
 const request = require('request');
 const forecast = (latitude, logitude, callback) => {
     const url = 'https://api.darksky.net/forecast/62b6f3d89534c3cabdee2373423dbd76/' + latitude + ',' + logitude;
-    request({ url: url, json: true }, (error, response) => {
+    request({ url, json: true }, (error, { body }) => {
         if (error) {
             callback('Unable to connect to weather service!', undefined);
-        } else if (response.body.error) {
+        } else if (body.error) {
             callback('Unable to find location', undefined);
         } else {
-            callback(undefined, response.body.daily.data[0].summary + ' It is currently ' + response.body.currently.temperature + ' degress out. There is a ' + response.body.currently.precipProbability + '% chance of rain.');
+            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degress out. There is a ' + body.currently.precipProbability + '% chance of rain.');
         }
     })
 
